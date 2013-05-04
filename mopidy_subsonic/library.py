fdffd9625de52fd1f4cfd7517a5e7700a9531208
Kevin Lemonnier <lemonnierk@ulrar.net>
2013-05-04 17:17:02 +0200

    Changed the library storage, now a lots faster in search

--- mopidy_subsonic/library.py
+++ mopidy_subsonic/library.py
@@ -9,9 +9,9 @@
 # Author: Kevin Lemonnier
 #           By: Kevin Lemonnier
 # Created: Wed Apr 17 19:54:44 2013 (+0200)
-# Last-Updated: Sat May  4 16:40:17 2013 (+0200)
+# Last-Updated: Sat May  4 17:15:28 2013 (+0200)
 # Version:
-#     Update #: 276
+#     Update #: 319
 
 # Change Log:
 #
@@ -35,9 +35,7 @@ logger = logging.getLogger('mopidy.backends.subsonic')
 class SubsonicLibraryProvider(base.BaseLibraryProvider):
     def __init__(self, *args, **kwargs):
         super(SubsonicLibraryProvider, self).__init__(*args, **kwargs)
-        self.ltracks = None
-        self.lartists = None
-        self.lalbums = None
+        self.library = None
 
     def find_exact(self, query=None, uris=None):
         return self.search(query=query, uris=uris)
@@ -66,55 +64,54 @@ class SubsonicLibraryProvider(base.BaseLibraryProvider):
 
     def refresh(self, uri=None):
         logger.info("Subsonic: refreshing library")
-        (self.ltracks, self.lartists, self.lalbums) = self.getAllTracks()
-
-    def search(self, query=None, uris=None):
-        if (self.ltracks == None or self.lartists == None or self.lalbums == None):
-            self.refresh()
-        if not query:
-            return (SearchResult(uri=None, tracks=self.ltracks[:], artists=self.lartists[:], albums=self.lalbums[:]))
-        else:
-            tracks = self.ltracks[:]
-            artists = []
-            albums = []
-            for track in tracks[:]:
-                if ("album" in query):
-                    if not (track.album.name == query["album"][0]):
-                        tracks.remove(track)
-                        continue
-                if ("artist" in query):
-                    remove = True
-                    for art in track.artists:
-                        if (art.name == query["artist"][0]):
-                            remove = False
-                    if (remove == True):
-                        tracks.remove(track)
-                        continue
-#                if ("date" in query):
-#                    if not (track.date == query["date"][0]):
-#                        tracks.remove(track)
-#                        continue
-                if (track.album not in albums):
-                    albums.append(track.album)
-                for artist in track.artists:
-                    if (artist not in artists):
-                        artists.append(artist)
-            res = SearchResult(uri=None, tracks=tracks, artists=artists, albums=albums)
-            return (res)
-
-    def getAllTracks(self):
-        tracks = []
-        artists = []
-        albums = []
+        self.library = {}
         res = self.backend.subsonic.search2("*:*", artistCount=0, albumCount=0, songCount=1000000000).get('searchResult2')
         for song in res.get('song'):
             artistlist = {Artist(uri="subsonic://artist=%s" % song.get('artist'), name=song.get('artist'))}
             oalbum = Album(uri="subsonic://album=%s" % song.get('album'), name=song.get('album'), artists=artistlist)
-            tracks.append(Track(uri="subsonic://%s:%d/%s/%s?id=%s&u=%s&p=enc:%s&c=mopidy&v=1.8" % (self.backend.subsonic._baseUrl, self.backend.subsonic._port, self.backend.subsonic._serverPath, 'download.view', song.get('id'), self.backend.subsonic._username, self.backend.subsonic._rawPass.encode("hex")), name=song.get('title'), artists=artistlist, album=oalbum, track_no=song.get('track'), disc_no=None, date=song.get('year'), length=song.get('duration'), bitrate=song.get('bitRate')))
-            artists.append(Artist(uri="subsonic://artist=%s" % song.get('artist'), name=song.get('artist')))
-            albums.append(oalbum)
-        return (tracks, artists, albums)
+            if (song.get('artist') not in self.library):
+                self.library[song.get('artist')] = {}
+            if (song.get('album') not in self.library[song.get('artist')]):
+                self.library[song.get('artist')][song.get('album')] = []
+            self.library[song.get('artist')][song.get('album')].append(Track(uri="subsonic://%s:%d/%s/%s?id=%s&u=%s&p=enc:%s&c=mopidy&v=1.8" % (self.backend.subsonic._baseUrl, self.backend.subsonic._port, self.backend.subsonic._serverPath, 'download.view', song.get('id'), self.backend.subsonic._username, self.backend.subsonic._rawPass.encode("hex")), name=song.get('title'), artists=artistlist, album=oalbum, track_no=song.get('track'), disc_no=None, date=song.get('year'), length=song.get('duration'), bitrate=song.get('bitRate')))
 
+    def search(self, query=None, uris=None):
+        if (self.library == None):
+            self.refresh()
+        if not query:
+            tracks = []
+            artists = []
+            albums = []
+            for artist in self.library:
+                artists.append(Artist(uri="subsonic://artist=%s" % artist, name=artist))
+                for album in self.library[artist]:
+                    albums.append(Album(uri="subsonic://album=%s" % album, name=album, artists=artists))
+                    for song in self.library[artist][album]:
+                        tracks.append(song)
+            return (SearchResult(uri=None, tracks=tracks, artists=artists, albums=albums))
+        else:
+            tracks = []
+            artists = []
+            albums = []
+            if ("album" in query and "artist" in "query"):
+                artists.append(Artist(uri="subsonic://artist=%s" % query["artist"][0], name=query["artist"][0]))
+                albums.append(Album(uri="subsonic://album=%s" % query["album"][0], name=query["album"][0], artists=artists))
+                tracks = self.library[query["artist"][0]][query["album"][0]]
+            elif ("artist" in query):
+                artists.append(Artist(uri="subsonic://artist=%s" % query["artist"][0], name=query["artist"][0]))
+                for album in self.library[query["artist"][0]]:
+                    albums.append(Album(uri="subsonic://album=%s" % album, name=album, artists=artists))
+                    for song in self.library[query["artist"][0]][album]:
+                        tracks.append(song)
+            elif ("album" in query):
+                for artist in self.library:
+                    if (query["album"][0] in artist):
+                        artists.append(artists.append(Artist(uri="subsonic://artist=%s" % artist, name=artist)))
+                        for song in library[artist][query["album"][0]]:
+                            tracks.append(song)
+                albums.append(Album(uri="subsonic://album=%s" % query["album"][0], name=query["album"][0], artists=artists))
+            res = SearchResult(uri=None, tracks=tracks, artists=artists, albums=albums)
+            return (res)
 
 #
 
